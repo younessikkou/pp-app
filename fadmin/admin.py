46d9da1a6875fbb009665276a0f55fe89fe7b1c0
unknown <ikkou557@gmail.com>
2023-12-06 15:38:27 +0100

    commit after updating database and adding pjs tables

--- fadmin/admin.py
+++ fadmin/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Fadmin
+from .models import Fadmin,Pjs, Type_pj
 
 admin.site.register(Fadmin)
+admin.site.register(Pjs)
+admin.site.register(Type_pj)
--- fadmin/models.py
+++ fadmin/models.py
@@ -1,16 +1,32 @@
 from django.db import models
 # Create your models here.
 
+
+class Type_pj(models.Model):
+    name_pj = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name_pj}"
+
+
+class Pjs(models.Model):
+    indice = models.CharField(max_length=3)
+    name = models.CharField(max_length=100)
+    reporter = models.ForeignKey(Type_pj, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
+
 class Fadmin(models.Model):
-    id = models.AutoField(primary_key=True)
     date_arr = models.DateField(auto_now_add=True, verbose_name="تاريخ التوصل")
     num_env = models.CharField(max_length=20, verbose_name="رقم الارسال")
     num_rapp_pj = models.CharField(max_length=20, verbose_name="رقم محضر الضابطة القضائية")
-    pj = models.CharField(max_length=30, verbose_name="الضابطة القضائية")
     num_rapp_pp = models.CharField(max_length=20 , verbose_name="رقم محضر النيابة العامة")
     num_plaint = models.CharField(max_length=20, verbose_name="رقم الشكاية")
     note = models.CharField(max_length=100, verbose_name="ملاحظات" , blank=True , null=True)
     search_indice = models.IntegerField(verbose_name="عدد مرات البحث", blank=True , null=True)
+    pj = models.OneToOneField(Pjs,on_delete=models.CASCADE,primary_key=True,verbose_name="الضابطة القضائية")
+
 
     def __str__(self):
-        return self.pj
+        return self.num_env
